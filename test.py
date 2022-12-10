afb7a36ff94f216aafd3e6e9e634a98afc9a3388
elaina <yaoguihhh@gmail.com>
2022-12-09 18:07:04 -0800

    refactor read.py into util and test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from Pyfhel import Pyfhel
 from utils import *
 
@@ -6,3 +7,7 @@ votes_raw = get_votes_raw()
 votes_count = get_votes_count()
 
 print(votes_count)
+
+#encode candidates : {HC - 1, DT - 0}
+enc_candidate = encoded_candidate()
+print(y.value_counts())
