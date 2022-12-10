66e22b3b56da927d7de051929a2e41cd307071bc
elaina <yaoguihhh@gmail.com>
2022-12-09 18:11:30 -0800

    refactor read.py into util and test

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from Pyfhel import Pyfhel
 from utils import *
 
@@ -10,4 +9,4 @@ print(votes_count)
 
 #encode candidates : {HC - 1, DT - 0}
 enc_candidate = encoded_candidate()
-print(y.value_counts())
+print(enc_candidate.value_counts())
--- utils.py
+++ utils.py
@@ -1,17 +1,18 @@
 import csv
 import category_encoders as ce
+import pandas as pd
 
 def get_votes_count():
     CSV_FILE = 'voter_data.csv'     # I've renamed the csv file in this directory for better readability
     vote_count = {}
     with open(CSV_FILE, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            candidate = row['presvote16post_2016']
-            if candidate in vote_count:
-            	vote_count[candidate] += 1
-            else:
-            	vote_count[candidate] = 1
+    for row in reader:
+        candidate = row['presvote16post_2016']
+        if candidate in vote_count:
+            vote_count[candidate] += 1
+        else:
+            vote_count[candidate] = 1
     return vote_count
 
 def get_votes_raw():
